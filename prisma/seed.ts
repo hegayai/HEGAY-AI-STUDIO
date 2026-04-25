da4176028918b03b18541a5e171e2c798592c072
hegayai <solobeesam@yahoo.co.uk>
2026-04-25 15:06:25 +0100

    Fix seed file to use project Prisma client

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,6 @@
 // prisma/seed.ts
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/src/core/db/client";
 import bcrypt from "bcryptjs";
-
-const prisma = new PrismaClient();
 
 async function main() {
   console.log("🌱 Seeding database...");
